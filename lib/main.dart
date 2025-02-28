bc3940b9c2abd305dd0bf0e7c1dc58f1c9c94a05
nikhilgoud003 <nikhilgoud2001@gmail.com>
2025-02-28 11:13:15 -0500

    Initial setup of the flutter app

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CalculatorApp());
+}
+
+class CalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      // home: Calculator(),
+    );
+  }
+}
